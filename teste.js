c602dcfb535ebf88ece353a4b13f3b397ef43b4e
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2023-01-07 20:54:29 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -2,24 +2,24 @@ const assert = require('assert');
 
 // código que define as variáveis newBtn, newText, listPend, listConc e limparBtn aqui
 
-// Teste para verificar se as tarefas são adicionadas corretamente à lista pendente
+// Aqui testo se as tarefas são adicionadas corretamente à lista pendente
 newText.value = 'Comprar pão';
 newBtn.click();
 assert.equal(listPend.children.length, 1);
 assert.equal(listPend.children[0].innerHTML, 'Comprar pão');
 
-// Teste para verificar se as tarefas são marcadas como concluídas corretamente
+// Teste para verificar se as tarefas são transferidas para 'concluídas'
 listPend.children[0].click();
 assert.equal(listPend.children.length, 0);
 assert.equal(listConc.children.length, 1);
 assert.equal(listConc.children[0].innerHTML, 'Comprar pão');
 
-// Teste para verificar se as tarefas são marcadas como pendentes novamente corretamente
+// Aqui verificar se as tarefas são transferidas de concluidas para pendentes  
 listConc.children[0].click();
 assert.equal(listPend.children.length, 1);
 assert.equal(listConc.children.length, 0);
 assert.equal(listPend.children[0].innerHTML, 'Comprar pão');
 
-// Teste para verificar se as tarefas são removidas corretamente quando o botão de limpar é clicado
+// Teste para verificar se as tarefas são deletadas com botão limpar 
 limparBtn.click();
 assert.equal(listConc.children.length, 0);
