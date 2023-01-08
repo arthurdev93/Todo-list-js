099ef7fef15105617b2654d3a355680f2918b274
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2023-01-08 10:02:14 -0300

    Update teste.js
    correção de erros

--- teste.js
+++ teste.js
@@ -1,29 +1,53 @@
 const assert = require('assert');
 
-//definição das variáveis newBtn, newText, listPend, listConc e limparBtn 
-const newText = document.getElementById("newText");
-const newBtn = document.getElementById("newBtn");
-const limparBtn = document.getElementById("limparBtn");
-const listPend = document.getElementById("listPend");
-const listConc = document.getElementById("listConc");
+// Cria os elementos de forma simulada
+const newText = { value: '' };
+const newBtn = {
+  addEventListener(event, callback) {
+    this.callback = callback;
+  },
+  click() {
+    this.callback;
+  }
+};
+const limparBtn = {
+  addEventListener(event, callback) {
+    this.callback = callback;
+  },
+  click() {
+    this.callback();
+  }
+};
+const listPend = {
+  children: [],
+  appendChild(element) {
+    this.children.push(element);
+  }
+};
+const listConc = {
+  children: [],
+  appendChild(element) {
+    this.children.push(element);
+  }
+};
 
 // Aqui testo se as tarefas são adicionadas corretamente à lista pendente
 newText.value = 'Comprar pão';
 newBtn.click();
 assert.equal(listPend.children.length, 1);
-assert.equal(listPend.children[0].innerHTML, 'comprar pão');
+assert.equal(listPend.children[0].innerHTML, 'Comprar pão');
 
 // Teste para verificar se as tarefas são transferidas para 'concluídas'
 listPend.children[0].click();
 assert.equal(listPend.children.length, 0);
 assert.equal(listConc.children.length, 1);
-assert.equal(listConc.children[0].innerHTML, 'comprar pão');
+assert.equal(listConc.children[0].innerHTML, 'Comprar pão');
 
 // Aqui verificar se as tarefas são transferidas de concluidas para pendentes  
 listConc.children[0].click();
 assert.equal(listPend.children.length, 1);
 assert.equal(listConc.children.length, 0);
-assert.equal(listPend.children[0].innerHTML, 'comprar pão');
+assert.equal(listPend.children[0].innerHTML, 'Comprar pão');
 
 // Teste para verificar se as tarefas são deletadas com botão limpar 
 limparBtn.click();
